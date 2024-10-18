50d5cae5515171c9cc449c15fbd41010f34d486f
night <luissiqueira760@gmail.com>
2024-10-17 21:50:15 -0300

    Criação dos templates dos jogos, mudanças no layout, sistema de alteração de logo de perfil e nome, adicionado o catálogo

--- main/scripts/config.js
+++ main/scripts/config.js
@@ -1,17 +1,3 @@
-// Função para trocar a imagem de perfil ao fazer upload
-document.getElementById('imageUpload').addEventListener('change', function() {
-    const file = this.files[0];
-
-    if (file) {
-        const reader = new FileReader();
-
-        reader.onload = function(e) {
-            document.getElementById('profileImage').setAttribute('src', e.target.result);
-        }
-
-        reader.readAsDataURL(file);
-    }
-});
 
 // Função para salvar as alterações do perfil
 function saveProfile() {
